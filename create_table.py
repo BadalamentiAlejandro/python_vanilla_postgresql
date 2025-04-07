b78533427c235d17410bf08dd58cb1fa9f405f3e
Alejandro Badalamenti <alejandro.badalamenti@gasparaguilo.com>
2025-04-07 12:49:11 +0200

    Added with statement and list_users

--- create_table.py
+++ create_table.py
@@ -1,18 +1,15 @@
 from conexion import connect
 
+
 def create_table():
-# Table creation.
-    conn = connect() # Creates connection.
-    cursor = conn.cursor() # Permits querys.
 
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS users(
-            id SERIAL PRIMATY KEY,
-            name VARCHAR(100) NOT NULL,
-            password VARCHAR(255) NOT NULL
-        )
-    """)
-
-    conn.commit()
-    cursor.close()
-    conn.close()
+    with connect() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS users(
+                    id SERIAL PRIMARY KEY,
+                    name VARCHAR(100) NOT NULL,
+                    password VARCHAR(255) NOT NULL
+                )
+            """)
+        conn.commit()
